982225057e7ff584fdfbc80bf7fe23bb41eb1707
Melike KILIC <melike180400@gmail.com>
2019-10-14 02:06:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,6 +35,6 @@ def using_delete(array,string)
 array.delete(string)
 end
 
-def using_delete_at(array,Integer)
-array.delete_at(Integer.index)
+def using_delete_at(array,integer)
+array.delete_at(integer.index)
 end
